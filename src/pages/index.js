a8270334a6b2c9bfd374881a97a3bc30699380ab
nick <nportello21@gmail.com>
2021-06-11 18:16:11 -0400

    adding a p to the image

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to Dead Elf Collectibles.</p>
     <p>New site is comming soon.</p>
+    <p>
     <StaticImage
       src="../images/DEC-Color-ElfAlone.svg"
       width={300}
@@ -18,7 +19,7 @@ const IndexPage = () => (
       formats={["AUTO", "WEBP", "AVIF"]}
       alt="Dead Elf"
       style={{ marginBottom: `1.45rem` }}
-    />
+    /></p>
   </Layout>
 )
 
