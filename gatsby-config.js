0c0a0336e466ce3becbd5b60f61d736703f5a0dc
nick <nportello21@gmail.com>
2021-06-11 18:12:07 -0400

    add needed changes
    title and layout changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Dead Elf Collectibles LLC.`,
+    description: `Dead Elf Collectibles is here to supply all your TCG needs`,
+    author: `@TheReal_deadelf`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#3A5F7D `,
       marginBottom: `1.45rem`,
     }}
   >
